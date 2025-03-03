3a05e71eab2adea23e53f96e5b34bbe93ffe2af2
leonardo fleitas <leoflalv.dev@gmail.com>
2025-03-03 14:37:21 +0100

    add request to route

--- src/message.rs
+++ src/message.rs
@@ -1,4 +1,7 @@
-use axum::{routing::get, Router};
+use axum::{
+    routing::{get, patch, post},
+    Router,
+};
 
 use crate::{common::types::ServiceTransport, GatewayState};
 
@@ -6,7 +9,13 @@ mod dtos;
 pub mod message_controller;
 
 pub fn message_routes<T: ServiceTransport>() -> Router<GatewayState<T>> {
-    let router = Router::new().route("/messages/{user_id}", get(message_controller::get_messages));
+    let router = Router::new()
+        .route("/messages/{user_id}", get(message_controller::get_messages))
+        .route("/message/send", post(message_controller::send_message))
+        .route(
+            "/message/{message_id}",
+            patch(message_controller::edit_message).delete(message_controller::delete_message),
+        );
 
     router
 }
